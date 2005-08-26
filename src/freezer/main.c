a97eafcb613e2a21e502995ad6a31d04d09d8c3e
Ricky Zhang <rickyzhang@gmail.com>
2005-08-26 01:02:00 -0400

    Add freezer prototype that do device matching for PPCI2CInterface user client.

--- src/freezer/main.c
+++ src/freezer/main.c
@@ -1,7 +1,27 @@
 #include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
+#include "I2CUserClient.h"
+
+#define IOCLASS_I2C "PPCI2CInterface"
 
 int main (int argc, const char * argv[]) {
-    // insert code here...
-    CFShow(CFSTR("Hello, World!\n"));
+    CFDictionaryRef     matchingDict = NULL;
+    io_iterator_t       iter = 0;
+    io_service_t        service = 0;
+    kern_return_t       kr;
+
+    // Create a matching dictionary that will find PPC I2C interface user client
+    kr =  IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter);
+    if(kr !== KERN_SUCCESS) {
+        printf("Failed to find PPC I2C interface!\n");
+        return -1;
+    }
+
+    // Iterate over all matching objects
+    while((service = IOIteratorNext(iter)) != 0) {
+        printf("Found PPCI2CInterface user client!\n")
+        IOObjectRelease(service);
+    }
+
     return 0;
 }
