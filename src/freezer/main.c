e1404fa8a85bc6961cbcc294599447a0a4170ace
Ricky Zhang <rickyzhang@gmail.com>
2016-08-27 05:31:45 -0400

    Print class name

--- src/freezer/main.c
+++ src/freezer/main.c
@@ -136,6 +136,9 @@ int pollADT746XChipViaI2C() {
             printf("Found I2C controller with class name %s matched!\n", className);
         else
             printf("Found I2C controller with unknown class name!\n");
+        if(0 == strcmp(className, kIOProviderClassValueIOPlatformDevice))
+            printf("Found a match to create a connection!\n");
+
         IOObjectRelease(service);
     }
 
